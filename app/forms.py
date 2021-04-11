8e4d64d40a197cc6f2e84bb56957b4f744ce0412
DangaRanga <jasonblue1626@gmail.com>
2021-04-11 02:10:31 -0500

    Added photo form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,17 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import TextAreaField
 
+# Validator imports
+from wtforms.validators import DataRequired
+
+
+class UploadForm(FlaskForm):
+    photo = FileField('photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png'], 'JPG and PNG Images only!')
+    ])
+
+    description = TextAreaField(
+        'Description', validators=[DataRequired()],
+        render_kw={"rows": 8, "cols": 50})
